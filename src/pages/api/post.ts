d05f57295a6a7c0d506625d12f2a3d882ab18b39
zhekabaila <zhekabaila@gmail.com>
2023-09-08 19:48:09 +0700

    fix: resolve error api

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   return res.status(200).json({
     name: 'zheka baila arkan',
   })
